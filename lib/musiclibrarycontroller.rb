f647d3c090554c5c3ac143692853cc7c09366180
SophiaGrace16 <sophia.g.white@outlook.com>
2020-09-04 19:31:23 -0400

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -6,8 +6,18 @@ class MusicLibraryController
     end
 
     def call
-        i = "input"
-        while i != "exit"
+      i = "input"
+      while i != "exit"
+      puts "Welcome to your music library!"
+            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of the artists in your library, enter 'list artists'."
+            puts "To list all of the genres in your library, enter 'list genres'."
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+            puts "To play a song, enter 'play song'."
+            puts "To quit, type 'exit'."
+            puts "What would you like to do?"
+            i = gets.chomp
             case i
             when "list songs"
               self.list_songs
@@ -21,19 +31,9 @@ class MusicLibraryController
               self.list_songs_by_genre
             when "play song"
               self.play_song
-            else
-            puts "Welcome to your music library!"
-            puts "To list all of your songs, enter 'list songs'."
-            puts "To list all of the artists in your library, enter 'list artists'."
-            puts "To list all of the genres in your library, enter 'list genres'."
-            puts "To list all of the songs by a particular artist, enter 'list artist'."
-            puts "To list all of the songs of a particular genre, enter 'list genre'."
-            puts "To play a song, enter 'play song'."
-            puts "To quit, type 'exit'."
-            puts "What would you like to do?"
-            i = gets.chomp
+            end
         end
-    end
+    
 
     def list_songs
         Song.all.sort_by {|song|song.name}.each.with_index(1) {|song,i| 
