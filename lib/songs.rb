e9a2767c53614f8c738e7304f09f5a952cba584f
Tabatha Slatton <valbatha@gmail.com>
2020-09-04 10:30:40 -0600

    Done.

--- lib/songs.rb
+++ lib/songs.rb
@@ -38,13 +38,13 @@ class Song
      genre.songs << self  unless genre.songs.include? self
  end 
 
- def self.find_by_name(name)
-    @@all.find {|song| song.name==name }
- end
+#  def self.find_by_name(name)
+#     @@all.find {|song| song.name==name }
+#  end
 
- def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
- end
+#  def self.find_or_create_by_name(name)
+#     self.find_by_name(name) || self.create(name)
+#  end
 
  def self.new_from_filename(filename)
     split_file = filename.split(/\s-\s|\.mp3/)
