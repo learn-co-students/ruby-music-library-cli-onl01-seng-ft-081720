a09ff8dbacf5554fdea91f313503fe3870e0529e
maxjacobzander <maxjacobzander@gmail.com>
2020-09-06 19:54:42 -0500

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -6,6 +6,10 @@ class MusicImporter
     end
 
     def files
-        
+        Dir.glob("#{path}/*.mp3").collect { | file | file.gsub("#{path}/", "") }
     end
+
+   def import
+    files.each {|filename| Song.create_from_filename(filename)}
+   end
 end
