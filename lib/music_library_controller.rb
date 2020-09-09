3a28347c8a80a48adc839c1f72905a2459ff5e10
mollymoog <mmkover@gmail.com>
2020-09-09 11:16:27 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,52 +8,48 @@ class MusicLibraryController
 
     end
 
-    def call
-        puts "Welcome to your music library!"
-
-        puts "To list all of your songs, enter 'list songs'."
-
-        puts "To list all of the artists in your library, enter 'list artists'."
-
-        puts "To list all of the genres in your library, enter 'list genres'."
-
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-
-        puts "To play a song, enter 'play song'."
-
-        puts "To quit, type 'exit'."
-
-        puts "What would you like to do?"
-
-
-        until gets == "exit"
-        # end
-
-        if gets = "list songs"
-            list_songs
-        end
-
-        if gets = "list artists"
-            list_artists
-        end
-
-        if gets = "list genres"
-            list_genres
-        end
-
-        if gets = "list artist"
-            list_songs_by_artist
-        end
-            
-        if gets = "list genre"
-            list_songs_by_genre
-        end
-
-        if gets = "play song"
-            play_song
-        end
+    def call 
+        input = ""
+        while input != "exit"
+        
+            puts "Welcome to your music library!"
+    
+            puts "To list all of your songs, enter 'list songs'."
+    
+            puts "To list all of the artists in your library, enter 'list artists'."
+    
+            puts "To list all of the genres in your library, enter 'list genres'."
+    
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+    
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+    
+            puts "To play a song, enter 'play song'."
+    
+            puts "To quit, type 'exit'."
+    
+            puts "What would you like to do?"
+    
+            input = gets.chomp
+            case input
+                when "list songs"
+                    list_songs
+        
+                when "list artists"
+                    list_artists
+        
+                when "list genres"
+                    list_genres
+        
+                when "list artist"
+                    list_songs_by_artist
+                    
+                when "list genre"
+                    list_songs_by_genre
+        
+                when "play song"
+                    play_song
+            end
         end
     end
 
